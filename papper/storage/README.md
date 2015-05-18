5f4a723b9414ce70ada66b436df12847fe7680af
Marc Helbling <marc.d.helbling@gmail.com>
2015-05-18 21:19:02 +0200

    Remove crawler/storage from papper

--- papper/storage/README.md
+++
@@ -1,5 +0,0 @@
-# Setting up postgres
-
-Need to create user/db manually:
-* `sudo -u postgres createuser 'Config.database['user'] --pwprompt`
-* `sudo -u postgres createdb --owner='Config.database['user']' --encoding=utf8 'Config.database['database']'`
--- papper/storage/config.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    database = {
-        'drivername': 'postgres',
-        'username': 'papper',
-        'password': 'kN/o09MB9aneNBAgCmz+wfiR2s2Ya/irN7NXRBWxGo4=',
-        'host': 'localhost',
-        'port': '5432',
-        'database': 'papper',
-    }
